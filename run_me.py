b4d58559fc84e13d6375b686c42220e55e569cd4
Ron King <ronk62@gmail.com>
2023-04-09 22:08:19 -0700

    provided comments about model execution behaviors

--- run_me.py
+++ run_me.py
@@ -5,8 +5,13 @@
 
 from Detector import *
 
+### this first model works fine so far on my machine with static images
 modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz"
+
+### this model is sketchy, crashes VS Code with OOM unless you bounce VS Code before running
 # modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d4_coco17_tpu-32.tar.gz"
+
+### can properly load or run this model
 # modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20210210/centernet_mobilenetv2fpn_512x512_coco17_od.tar.gz"
 
 classFile = "coco.names"
